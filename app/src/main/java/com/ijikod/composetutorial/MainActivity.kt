97497f116d187a7da58e3f3d852bfc185413f9e1
samdankudjodji <samdantettey@gmail.com>
2022-09-16 08:20:02 +0100

    added animation to list conversation

--- app/src/main/java/com/ijikod/composetutorial/MainActivity.kt
+++ app/src/main/java/com/ijikod/composetutorial/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,7 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -62,7 +63,14 @@ fun MessageCard(msg: Message) {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = msg.author,
             color = MaterialTheme.colors.secondaryVariant,
             style = MaterialTheme.typography.subtitle2)
@@ -71,9 +79,11 @@ fun MessageCard(msg: Message) {
             Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
                 Text(text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2)
             }
-
         }
     }
 }
