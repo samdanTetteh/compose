08f2edc4a3ed085c79f320e9652c2e70a47c6079
samdankudjodji <samdantettey@gmail.com>
2022-09-16 08:05:38 +0100

    implemented lazy colomn composable

--- app/src/main/java/com/ijikod/composetutorial/MainActivity.kt
+++ app/src/main/java/com/ijikod/composetutorial/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,6 +19,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ijikod.composetutorial.data.Message
+import com.ijikod.composetutorial.data.SampleData
 import com.ijikod.composetutorial.ui.theme.ComposeTutorialTheme
 
 
@@ -27,14 +31,13 @@ class MainActivity : ComponentActivity() {
             ComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    MessageCard(Message("Android Developer", "Trying out Jetpack Compose"))
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
     }
 }
 
-data class Message(val author: String, val body: String)
 
 @Composable
 fun Greeting(name: String) {
@@ -98,5 +101,22 @@ fun PreviewMessageCard(){
 fun DefaultPreview() {
     ComposeTutorialTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn {
+        items(messages) {message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    ComposeTutorialTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
