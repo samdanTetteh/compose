8f1640415e94e9034d14ed7d5ac477d8e3279ecf
samdankudjodji <samdantettey@gmail.com>
2022-09-22 07:53:40 +0100

    added rows and colums

--- app/src/main/java/com/ijikod/composetutorial/ComposeBasicsActivity.kt
+++ app/src/main/java/com/ijikod/composetutorial/ComposeBasicsActivity.kt
@@ -3,9 +3,9 @@ package com.ijikod.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,22 +25,36 @@ class ComposeBasicsActivity : ComponentActivity() {
 
 
     @Composable
-    private fun MyApp(){
+    private fun MyApp(names: List<String> = listOf("World", "Compose")) {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-            Greeting(name = "Android")
+            Column(modifier = Modifier.padding(vertical = 4.dp)) {
+                for (name in names) {
+                    Greeting(name = name)
+                }
+            }
         }
     }
 
     @Composable
     private fun Greeting(name: String){
-        Surface(color = MaterialTheme.colors.primary) {
-            Text(text = "Hello $name", modifier = Modifier.padding(24.dp))
+        Surface(color = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "Hello,")
+                    Text(text = name)
+                }
+                
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Text(text = "Show more")
+                }
+            }
         }
 
     }
 
-    @Preview(showBackground = true, name = "Text Preview")
+    @Preview(showBackground = true, name = "Text Preview", widthDp = 320)
     @Composable
     fun DefaultPreview() {
         ComposeTutorialTheme { MyApp() }
