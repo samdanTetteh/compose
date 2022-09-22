c8ba1a8e4445818a1cf4eca2cf7018e9761561c1
samdankudjodji <samdantettey@gmail.com>
2022-09-22 06:53:56 +0100

    added new basics activity

---
+++ app/src/main/java/com/ijikod/composetutorial/ComposeBasicsActivity.kt
@@ -0,0 +1,50 @@
+package com.ijikod.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ijikod.composetutorial.ui.theme.ComposeTutorialTheme
+
+class ComposeBasicsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting(name = "Android")
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    private fun Greeting(name: String){
+        Surface(color = MaterialTheme.colors.primary) {
+            Text(text = "Hello $name", modifier = Modifier.padding(24.dp))
+        }
+
+    }
+
+    @Preview(showBackground = true, name = "Text Preview")
+    @Composable
+    fun Preview(){
+        ComposeTutorialTheme {
+            Greeting(name = "Samdan")
+        }
+    }
+}
