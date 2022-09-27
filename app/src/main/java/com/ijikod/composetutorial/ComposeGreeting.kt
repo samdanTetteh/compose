b3fdd427afc54d6bf48dd70b7e705c402136961e
Samdan Kudjodji <samdantettey@gmail.com>
2022-09-27 07:09:34 +0100

    added padding animation

--- app/src/main/java/com/ijikod/composetutorial/ComposeGreeting.kt
+++ app/src/main/java/com/ijikod/composetutorial/ComposeGreeting.kt
@@ -1,5 +1,8 @@
 package com.ijikod.composetutorial
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -7,9 +10,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,23 +32,31 @@ class ComposeGreeting {
 
     @Composable
     private fun Greeting(name: String) {
-        val extended = remember { mutableStateOf(false) }
+        var extended by remember { mutableStateOf(false) }
 
-        val extraPadding  = if (extended.value) 48.dp else 0.dp
+        val extraPadding by animateDpAsState (
+            if (extended) 48.dp else 0.dp,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
 
         Surface(color = MaterialTheme.colors.primary,
             modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
             Row(modifier = Modifier.padding(all = 24.dp)) {
-                Column(modifier = Modifier.weight(1f)) {
+                Column(modifier = Modifier.weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                     Text(text = "Hello,")
                     Text(text = name)
                 }
 
-                OutlinedButton( modifier = Modifier.padding(bottom = extraPadding),
-                    onClick = { extended.value = !extended.value }
+                OutlinedButton( modifier = Modifier.padding(bottom = extraPadding.coerceAtLeast(0.dp)),
+                    onClick = { extended = !extended }
                 )
+
                 {
-                    Text( if(extended.value)  "Show less" else "Show more" )
+                    Text( if (extended)  "Show less" else "Show more" )
                 }
             }
         }
