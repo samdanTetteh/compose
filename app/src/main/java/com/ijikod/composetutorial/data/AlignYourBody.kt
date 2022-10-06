6021009715364bc484a4d2a62e5b4770691259a2
Samdan Kudjodji <samdantettey@gmail.com>
2022-10-06 07:29:49 +0100

    added sample data for strolling row

---
+++ app/src/main/java/com/ijikod/composetutorial/data/AlignYourBody.kt
@@ -0,0 +1,3 @@
+package com.ijikod.composetutorial.data
+
+data class AlignYourBody (val imageDrawable : Int, val text: Int)
---
+++ app/src/main/java/com/ijikod/composetutorial/data/AlignYourBodySampleData.kt
@@ -0,0 +1,32 @@
+package com.ijikod.composetutorial.data
+
+import com.ijikod.composetutorial.R
+
+object AlignYourBodySampleData {
+
+    val alignBodySample = listOf(
+        AlignYourBody(
+            R.drawable.ab1_inversions,
+            R.string.ab1_inversions
+        ),
+
+        AlignYourBody(
+            R.drawable.ab2_quick_yoga,
+            R.string.ab2_quick_yoga
+        ),
+
+        AlignYourBody(
+            R.drawable.ab3_stretching,
+            R.string.ab3_stretching
+        ),
+
+        AlignYourBody(
+            R.drawable.ab4_tabata,
+            R.string.ab4_tabata
+        ),
+
+        AlignYourBody(
+            R.drawable.ab6_pre_natal_yoga,
+            R.string.ab6_pre_natal_yoga)
+    )
+}
