f88bf3dac1e6d875b24e105491201e166a43a8f5
Samdan Kudjodji <samdantettey@gmail.com>
2022-10-15 04:35:35 +0100

    described a somple UI composition based on UI state

--- app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WaterCounter.kt
@@ -14,8 +14,11 @@ import com.ijikod.composetutorial.R
 fun WaterCounter(modifier: Modifier = Modifier){
     var count by remember { mutableStateOf(0) }
     Column(modifier = modifier.padding(16.dp)) {
-        Text(text = stringResource(id = R.string.glasses_taken_text, count))
-        Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text(text = stringResource(id = R.string.glasses_taken_text, count))
+        }
+
+        Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp), enabled = count < 10) {
                Text(text = stringResource(R.string.add_one_btn_txt))
         }
     }
