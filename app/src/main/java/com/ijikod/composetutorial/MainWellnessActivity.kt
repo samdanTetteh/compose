cf55b575e2c6cfdf4c7f08306fa393da3f78d0fe
Samdan Kudjodji <samdantettey@gmail.com>
2022-10-15 03:37:52 +0100

    implemented compose state with remember

--- app/src/main/java/com/ijikod/composetutorial/MainWellnessActivity.kt
+++ app/src/main/java/com/ijikod/composetutorial/MainWellnessActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ijikod.composetutorial.basicstatecodelab.WellnessScreen
-import com.ijikod.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainWellnessActivity: ComponentActivity() {
 
--- app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WaterCounter.kt
@@ -1,9 +1,10 @@
 package com.ijikod.composetutorial.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -11,7 +12,12 @@ import com.ijikod.composetutorial.R
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
-    val count = 0
-    Text(text = stringResource(id = R.string.glasses_taken_text, count),
-    modifier = modifier.padding(16.dp))
+    var count by remember { mutableStateOf(0) }
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = stringResource(id = R.string.glasses_taken_text, count))
+        Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp)) {
+               Text(text = stringResource(R.string.add_one_btn_txt))
+        }
+    }
+
 }
