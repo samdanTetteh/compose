b45eaa67eba41e778746d6bcfdf6aff0767061a7
Samdan Kudjodji <samdantettey@gmail.com>
2022-11-11 06:24:59 +0000

    remember composition

--- app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.ijikod.composetutorial.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -15,12 +16,24 @@ fun WaterCounter(modifier: Modifier = Modifier){
     var count by remember { mutableStateOf(0) }
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(taskName = "Have you taken your 15 minute walk today?",
+                onClose = { showTask = false})
+            }
             Text(text = stringResource(id = R.string.glasses_taken_text, count))
         }
 
-        Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp), enabled = count < 10) {
-               Text(text = stringResource(R.string.add_one_btn_txt))
+        Row(modifier = modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text(text = stringResource(R.string.add_one_btn_txt))
+            }
+
+            Button(onClick = { count = 0 }, modifier = modifier.padding(start = 8.dp)) {
+                Text(text = stringResource(id = R.string.clear_water_count_txt))
+            }
         }
+
     }
 
 }
---
+++ app/src/main/java/com/ijikod/composetutorial/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,35 @@
+package com.ijikod.composetutorial.basicstatecodelab
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier, onClose: () -> Unit) {
+
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(text = taskName, modifier = Modifier
+            .weight(1f)
+            .padding(start = 16.dp))
+
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultView(){
+    WellnessTaskItem(taskName = "Sam", onClose = {})
+}
